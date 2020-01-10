2b37f6c5fb931f58e1f7c58afc93233ad55e6f8b
Dante <nogueradante@gmail.com>
2020-01-09 23:38:19 -0300

    ...

--- Common.hs
+++ Common.hs
@@ -1,24 +1,46 @@
 module Common where
-import Prelude hiding (repeat)
+import Prelude
 import Data.Int
-import Data.Stream
-
-type Exp = [Op]
-
-data Machine a = Machine { tape :: Stream a
-                         , pointer :: a
-                         } deriving Show
-
-newMachine :: Machine Int8
-newMachine = Machine (repeat 0) 0
+import Control.Applicative (Applicative(..))
+import Control.Monad (liftM, ap)
 
 
+type Prog = [Comm]
 
-data Op = While Exp
-        | IncP
-        | DecP
-        | IncB
-        | DecB
-        | W
-        | R
+data Comm = While Prog
+          | IncP
+          | DecP
+          | IncB
+          | DecB
+          | W
+          | R
     deriving Show
+
+
+data Machine a = Machine [a] a [a] [a] [a]
+
+-- Para calmar GHC--
+instance Functor ControlMachine where
+    fmap = liftM
+instance Applicative ControlMachine where
+    pure = return
+    (<*>) = ap
+--------------------
+
+data Error a = Raise String | Return a
+
+newtype ControlMachine a = ControlMachine {runControlMachine :: Error a}
+
+instance Monad ControlMachine where
+    return x = ControlMachine (Return x)
+    m >>= f  =  case runControlMachine m of
+                    Raise e -> ControlMachine (Raise e)
+                    Return x -> f x
+
+runE, sizeE, readE :: ControlMachine a
+runE = ControlMachine (Raise "Runtime Error")
+sizeE = ControlMachine (Raise "Invalid Size")
+readE = ControlMachine (Raise "Empty Stream")
+
+mkMachine :: Int -> [Int8] -> Machine Int8
+mkMachine size inp = Machine [] 0 (replicate (size - 1) 0) inp []
--- Eval.hs
+++ Eval.hs
@@ -1,3 +1,29 @@
 module Eval where
 import Common
-import Data.Stream.Tape
+import Data.Int
+
+
+
+evalComm :: Comm -> Machine Int8 -> ControlMachine (Machine Int8)
+evalComm (While p) m@(Machine _ c _ _ _) = if c == 0 then return m else evalProg' (p ++ [While p]) m
+evalComm DecP (Machine [] _ _ _ _) = runE
+evalComm DecP (Machine  (l : ls) c rs is os) = return (Machine ls l (c : rs) is os)
+evalComm IncP (Machine _ _ [] _ _) = runE
+evalComm IncP (Machine ls c (r : rs) is os) = return (Machine (c : ls) r rs is os)
+evalComm IncB (Machine ls c rs is os) = return (Machine ls (c + 1) rs is os)
+evalComm DecB (Machine ls c rs is os) = return (Machine ls (c - 1) rs is os)
+evalComm W (Machine ls c rs is os) = return (Machine ls c rs is (c : os))
+evalComm R (Machine _ _ _ [] _) = readE
+evalComm R (Machine ls c rs is os) = return (Machine ls c rs is (c : os))
+
+
+evalProg :: Prog -> Int -> [Int8] -> ControlMachine (Machine Int8)
+evalProg p size inp = if size <= 0 then sizeE
+    else evalProg' p (mkMachine size inp)
+
+evalProg' :: Prog -> Machine Int8 -> ControlMachine (Machine Int8)
+evalProg' p m = case p of
+    [] -> return m
+    (op : ops) -> do
+        m' <- evalComm op m
+        evalProg' ops m'
