dca54d78c5289c5a000430b8471682c2346a7bd8
Dante <nogueradante@gmail.com>
2020-01-12 19:23:35 -0300

    ...

--- Common.hs
+++ Common.hs
@@ -1,9 +1,30 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Common where
 import Prelude
 import Data.Int
 import Control.Applicative (Applicative(..))
 import Control.Monad (liftM, ap)
+import Control.Monad.Trans.Class
+import Data.Word
 
+-- Para calmar GHC--
+instance Functor OperateMachine where
+    fmap = liftM
+instance Applicative OperateMachine where
+    pure = return
+    (<*>) = ap
+--------------------
+
+-- Para calmar GHC--
+instance Monad m => Functor (OperateMachineT m) where
+    fmap = liftM
+instance Monad m => Applicative (OperateMachineT m) where
+    pure = return
+    (<*>) = ap
+--------------------
 
 type Prog = [Comm]
 
@@ -16,31 +37,45 @@ data Comm = While Prog
           | R
     deriving Show
 
+data Machine a = Machine [a] a [a] [a] [a] deriving Show
 
-data Machine a = Machine [a] a [a] [a] [a]
+data Error a = Raise String | Return a deriving Show
 
--- Para calmar GHC--
-instance Functor ControlMachine where
-    fmap = liftM
-instance Applicative ControlMachine where
-    pure = return
-    (<*>) = ap
---------------------
+newtype OperateMachine a = OperateMachine { runOperateMachine :: Error a } deriving Show
 
-data Error a = Raise String | Return a
-
-newtype ControlMachine a = ControlMachine {runControlMachine :: Error a}
-
-instance Monad ControlMachine where
-    return x = ControlMachine (Return x)
-    m >>= f  =  case runControlMachine m of
-                    Raise e -> ControlMachine (Raise e)
+instance Monad OperateMachine where
+    return x = OperateMachine (Return x)
+    m >>= f  =  case runOperateMachine m of
+                    Raise e -> OperateMachine (Raise e)
                     Return x -> f x
 
-runE, sizeE, readE :: ControlMachine a
-runE = ControlMachine (Raise "Runtime Error")
-sizeE = ControlMachine (Raise "Invalid Size")
-readE = ControlMachine (Raise "Empty Stream")
+pointE,readE :: OperateMachine a
+pointE = OperateMachine (Raise "Pointer out of bounds")
+readE = OperateMachine (Raise "Empty Stream")
 
-mkMachine :: Int -> [Int8] -> Machine Int8
+sizeE :: Error a
+sizeE = Raise "Invalid Size"
+
+mkMachine :: Int -> [Word8] -> Machine Word8
 mkMachine size inp = Machine [] 0 (replicate (size - 1) 0) inp []
+
+
+-- Para la Maquina interactiva --
+
+newtype OperateMachineT m a = OperateMachineT { runOperateMachineT :: m (Error a) }
+
+instance Monad m => Monad (OperateMachineT m) where
+    return x = OperateMachineT (return (Return x))
+    m >>= f  = OperateMachineT $ do errval <- runOperateMachineT m
+                                    case errval of
+                                        Raise e  -> return (Raise e)
+                                        Return x -> runOperateMachineT (f x)
+
+instance MonadTrans OperateMachineT where
+    lift = OperateMachineT . (liftM Return)
+
+pointEI :: Monad m => OperateMachineT m a
+pointEI = OperateMachineT $ return (Raise "Pointer out of bounds")
+
+sizeEI :: IO (Error a)
+sizeEI = return (Raise "Invalid Size")
